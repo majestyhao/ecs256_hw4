f2599e2b18dfbe58a1cd7df54963ff499964cd37
Hao Fu <jamesfuhao@gmail.com>
2014-03-02 00:07:06 -0800

    P1

--- P1.R
+++ P1.R
@@ -1,1 +1,8 @@
 # explore the bias of an approximate regression model
+verification <- function(nrep) {
+  X <- runif(nrep, 0, 1) # sample of X
+  Y <- X ^ 0.75
+  summary(lm(Y ~ X + 0))
+}
+
+verification(500000)
--- book_eg.R
+++ book_eg.R
@@ -1,25 +1,9 @@
 players <- read.table("Baseball.dat", header = T) # page 318
-# head(players)
-# players$Weight
-
-function <- verifyEstimate() {
-  w <- 0
-  for (i in 1:nrow(players)) {
-    # print(players$Weight[i])
-    if (i == 641)
-      w <- w + 0
-    else
-      w <- w + players$Weight[i]
-  }
-  
-  meanW <- (w/(nrow(players) - 1))
-  meanH <- (sum(players$Height)/nrow(players))
-  print(-155.092 + 4.841 * meanH)
-  print(meanW)
-}
 
 # sample estimate is inside the Coeff - Estimate: 
-#  sample's mean height based on sample's mean weight
-# verification
-# verifyEstimate()
-summary(lm(players$Weight ~ players$Height)) 
+# lm() 通过 sample data Xi, Yi给出拟合的 Y 的函数
+# 即对 population's regression function 的拟合 function
+summary(lm(players$Weight ~ players$Height)) # only consider Height
+lmout <- lm(players$Weight ~ players$Height + players$Age)
+summary(lmout) # add Age
+vcov(lmout)
