907f0982cb5a97ddf840a6d739e4d86393187ada
Hao Fu <jamesfuhao@gmail.com>
2014-03-03 19:16:56 -0800

    almost done P2a

--- Parsimony.R
+++ Parsimony.R
@@ -1,35 +1,94 @@
 # Explore a new approach for Model selection probelm: select predictor
-# 1. choose a prediction accuracy criterion and k: R2 & AIC, incooroprates 
+# 1. choose a prediction accuracy criterion and k: R2 & AIC, incorprates 
 # protection against overfitting; 
 # 2. starting with the full model (use all avail preodicator)， del one 
-# at a time as long as the PAC doesn't deteriorate much (how to justify it?)
+# at a time as long as the PAC doesn't deteriorate much (k)
 # 3. handle even n is large
 prsm <- function(Y, X, k = 0.01, predacc = ar2, crit = NULL, printdel = F) {
   # y: The vector of response values in the data.
   # x: The matrix of predictor values.
   # k: The "almost" measure, discussed above.
   # predacc: The PAC function, with arguments y and x.
-  # crit: Either "max" or "min", depending on whether good values of the PAC
-  # are large or small.
-  # printdel: If TRUE, gives a "progress report" as the computation proceeds. 
-  # Each time a predictor is deleted, the new value of the PAC is printed out, 
-  # along with the name of the variable.
-  PAC <- rep(0, ncol(X))
+  # crit: Either "max" or "min"
+  # printdel: "progress report" as the computation proceeds. 
+  
+  PAC <- rep(0, ncol(X) + 1)
+  pN <- ncol(X)
+  Xb <- X # backup for X
+  drop <- cbind(0)
+  startPoint <- pN/2
+  delFlag <- FALSE
+  conFlag <- TRUE
+  i <- startPoint
+  reserve <- NULL
+  
   if (identical(predacc, ar2)) {
-    for (i in ncol(X):1) {
-      PAC[i] <- ar2(Y, X)
-      X[, i] = NULL
-    }
-    if (printdel == T)
-      print(PAC)
-  }  else {
-    for (i in ncol(X):1) {
-      PAC[i] <- aiclogit(Y, X)
-      X[, i] = NULL
-    }
-    if (printdel == T)
-      print(PAC)
+    PAC[pN + 1] <- ar2(Y, X)
+  } else {
+    PAC[pN + 1] <- aiclogit(Y, X)
   }
+  cat("full outcome = ")
+  cat(PAC[pN + 1])
+  pac <- PAC[pN + 1]
+  cat("\n")
+  
+  while(conFlag) { 
+    if (i == startPoint - 1)
+      conFlag <- FALSE
+    # reconstruct/recombine X
+    X <- NULL
+    # X <- Xb
+    # X[, i] <- NULL # NULL is incorrect...why?
+    flag <- FALSE
+    for (j in 1:pN) {
+      if (j == i)
+        flag <- TRUE
+      if (ncol(drop) > 1) {
+        for(m in 2:ncol(drop)) {
+          if (j == drop[1, m]) {
+            flag <- TRUE
+          }
+        }
+      }
+      if (flag == FALSE) {
+        X <- cbind(X, Xb[, j])
+      }
+      flag <- FALSE       
+    }
+    
+    if (identical(predacc, ar2)) {
+      PAC[i] <- ar2(Y, X)
+      if (PAC[i]/pac >= 1 - k) {
+        delFlag <- TRUE
+      } else {
+        reserve <- c(reserve, i)
+      } 
+    } else {
+      PAC[i] <- aiclogit(Y, X)
+      if (PAC[i]/pac < 1 + k) {
+        delFlag <- TRUE
+      } else {
+        reserve <- c(reserve, i)   
+      }
+    }
+    
+    if (delFlag == TRUE) {
+      # delete the predicator
+      # Xb <- X
+      pac <- PAC[i]
+      drop <- cbind(drop, i)
+      cat("deleted ")
+      cat(i)
+      cat(": \n new outcome: ")
+      cat(PAC[i])         
+      cat("\n")
+    }  
+    delFlag <- FALSE
+    i <- i + 1
+    if (i == pN + 1)
+      i <- 1
+  } 
+  print(reserve)
 }
 
 # adjusted R^2
@@ -40,31 +99,14 @@ ar2 <- function(Y, X) {
   pN <- ncol(X) # less function call.. predictor number
   sampleN <- nrow(X) # sample size
   residualCol <- 8 - pN
-  # print(residualCol)
   if (residualCol != 0) {
     for (i in 1:residualCol) {
-      X[, pN + i] = c(rep(0, sampleN))
-      # X[, i] = NULL
+      X <- cbind(X, rep(0, sampleN))
     }
   }
-  # print(X)
-  lmout <- lm(Y ~ X[, 1] + X[, 2] + X[, 3] + X[, 4] + X[, 5] + 
-                X[, 6] + X[, 7] + X[, 8])
-  # summary()
-  Yh <- rep(0, sampleN) # Y-hat
-  for (j in 1:sampleN) {
-    for (i in 1:pN) {
-      if (i == 0) {
-        Yh[j] <- lmout$coefficients[i] # intercept
-      } else {
-        Yh[j] <- Yh[j] + lmout$coefficients[i + 1] * X[j, i]
-      }       
-    }
-  }
-  r2 <- 1 - sum((Y - Yh) ^ 2)/sum((Y - mean(Y)) ^ 2)
-  ar2 <- 1 - (1 - r2) * (sampleN - 1)/(sampleN - pN - 1) # adjusted r2
-  # print(r2)
-  return(ar2) 
+  lmout <- summary(lm(Y ~ X[, 1] + X[, 2] + X[, 3] + X[, 4] + X[, 5] + 
+                        X[, 6] + X[, 7] + X[, 8]))  
+  return(lmout$adj.r.squared) 
 }
 
 aiclogit <- function(Y, X) {
@@ -73,26 +115,23 @@ aiclogit <- function(Y, X) {
   pN <- ncol(X) # less function call..
   sampleN <- nrow(X)
   residualCol <- 8 - pN
-  # print(residualCol)
   if (residualCol != 0) {
     for (i in 1:residualCol) {
-      X[, pN + i] = c(rep(0, sampleN))
-      # X[, i] = NULL
+      X <- cbind(X, rep(0, sampleN))
     }
   }
-  # print(X)
   glmout <- glm(Y ~ X[, 1] + X[, 2] + X[, 3] + X[, 4] + X[, 5] + 
-                  X[, 6] + X[, 7] + X[, 8])
-  # print(summary(glmout))
-  # print(glmout)
-  aic <- AIC(glmout)
+                  X[, 6] + X[, 7] + X[, 8], family = binomial)
+  aic <- summary(glmout)$aic # AIC(glmout)
   return(aic) # well, just habbit..
 }
 
+
 main <- function() {
-  pm <- read.table("pima-indians-diabetes.data", header = F, sep = ",")
+  pm <- read.table("pima-indians-diabetes.data", header = T, sep = ",", row.names=NULL)
   prsm(pm[, 9], pm[, 1:8], predacc = ar2, printdel = T)
-  # prsm(pm[,9], pm[, 1:8], predacc = aiclogit, printdel = F)
+  cat("\n")
+  prsm(pm[, 9], pm[, 1:8], predacc = aiclogit, printdel = T)
 }
 
 main()
--- test.R
+++ test.R
@@ -1,6 +1,19 @@
-pm <- read.table("pima-indians-diabetes.data", header = F, sep = ",")
+pm <- read.table("pima-indians-diabetes.data", header = T, sep = ",", row.names=NULL)
+# print(head(pm))
 Y <- pm[, 9]
-X <- pm[, 1:8]
-# x <- rbind(rep(0, nrow(pm)))
-# x <- c(rep(0, nrow(pm)))
-ar2(pm[,9], pm[, 1:8])
+a <- 0
+# if (ncol(a) != NULL)
+# print(rep(0, ncol(a)))
+# a <- cbind(1, 2, 3)
+# print(ncol(a))
+X <- NULL
+# X <- cbind(X, pm[, 1], pm[, 2], pm[, 6], pm[, 7])
+X <- cbind(X, pm[, 1], pm[, 2], pm[, 3], pm[, 6], pm[, 7], pm[,8])
+# print(ncol(X))
+
+
+
+# X[, 4] = NULL 
+# X[, 5] = NULL
+print(aiclogit(Y, X))
+
